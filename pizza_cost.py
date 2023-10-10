544ff30e1220ea58cf3fa5f55dd292c858f2c354
Meshach Wallace <meshach.wallace@stu.ocsb.ca>
2023-10-10 15:27:53 +0000

    adding Pizzacost

---
+++ pizza_cost.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by: Meshach Wallace
+# Created on: Oct. 1, 2023
+# This program calculates and displays the total of pizza of any diameter
+
+def main():
+    print ("For a pizza that has a diameter of")
+    diameter = input("Enter diameter in inches:")  
+    float(diameter)
+    print ("Diameter is:" + diameter)
+    subtotal = float(diameter) * 1.5+4.25
+   
+
+
+if __name__ == "__main__":
+  main()
